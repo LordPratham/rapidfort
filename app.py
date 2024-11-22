964e28c0d5c2f0fe38f2a85fb3445b746ccfdcc6
Pratham Agarwal <pratham@Abhisheks-MacBook-Pro.local>
2024-11-22 16:12:42 +0530

    removed debug tag

--- app.py
+++ app.py
@@ -84,5 +84,5 @@ def add_password():
     return {"error": "File not found"}, 404
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
 
