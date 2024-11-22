9458285683ed5366286644d6ac659720da893865
Pratham Agarwal <pratham@Abhisheks-MacBook-Pro.local>
2024-11-22 16:34:23 +0530

    fixed issurs

--- app.py
+++ app.py
@@ -11,8 +11,9 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
-UPLOAD_FOLDER = "uploads"
+UPLOAD_FOLDER = os.path.join(os.getcwd(), "uploads")
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
 
 @app.route('/')
 def home():
