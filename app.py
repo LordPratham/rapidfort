917c3c6b4382501e497c8aec04318336465545a4
Pratham Agarwal <pratham@Abhisheks-MacBook-Pro.local>
2024-11-22 19:15:02 +0530

    render

--- app.py
+++ app.py
@@ -89,5 +89,6 @@ def add_password():
     return {"error": "File not found"}, 404
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
